3f3320327cf82d5d1fb7f488ab0870e6cfc6b857
Cristian Plasencia <plc.cristian@gmail.com>
2021-03-29 17:04:42 +0100

    login implementado

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,15 @@
 import { Request, Response, Router } from "express";
 import { User } from "../entities/User";
-import { validate } from 'class-validator';
+import { validate, isEmpty } from 'class-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
 
 const register = async (req: Request, res: Response) => {
     const { email, username, password } = req.body;
 
     try {
-        //TODO: validar datos
+        //validar datos
         let errors: any = {}
         const emailUser = await User.findOne({ email })
         const usernameUser = await User.findOne({ username })
@@ -22,7 +25,7 @@ const register = async (req: Request, res: Response) => {
             return res.status(400).json(errors);
         }
 
-        //TODO: crear usuario
+        //crear usuario
         const user = new User({ email, username, password })
 
         errors = await validate(user);
@@ -31,7 +34,7 @@ const register = async (req: Request, res: Response) => {
         }
 
         await user.save();;
-        //return usuario
+        //devuelve usuario
         return res.json(user)
     } catch (err) {
         console.log(err)
@@ -39,7 +42,64 @@ const register = async (req: Request, res: Response) => {
     }
 }
 
+const login = async (req: Request, res: Response) => {
+ const { username, password } = req.body;
+ 
+ try {
+    const errors: any = {}
+    if(isEmpty(username)) errors.username = 'El campo usuario está vacio'
+    if(isEmpty(password)) errors.password = 'El campo contraseña está vacio'
+    if(Object.keys(errors).length > 0) {
+        return res.status(400).json(errors);
+    }
+
+     const user = await User.findOne({ username })
+
+     if(!user) return res.status(404).json({ error: 'Usuario no encontrado'})
+
+     const passwordMatches = await bcrypt.compare(password, user.password)
+
+     if(!passwordMatches){
+         return res.status(401).json({ password: 'Contraseña incorrecta' })
+     }
+
+     const token = jwt.sign({ username }, process.env.JWT_SECRET)
+
+     //guarda el token en una cookie
+     res.set('Set-Cookie', cookie.serialize('token', token, {
+         httpOnly: true,
+         secure: process.env.NODE_ENV === 'production',
+         sameSite: 'strict',
+         maxAge: 3600,
+         path: '/'
+     }))
+
+     return res.json(token);
+ } catch (err) {
+     
+ }
+}
+
+const me = async (req: Request, res: Response) => {
+    try {
+        const token = req.cookies.token;
+        if(!token) throw new Error('Sin autentificar')
+
+        const { username }:any = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findOne({ username })
+
+        if(!user) throw new Error('Sin autentificar')
+
+        return res.json(user);
+    } catch (err) {
+        return res.status(401).json({error: err.message})
+    }
+}
+
 const router = Router()
 router.post('/register', register)
+router.post('/login', login)
+router.get('/me', me)
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,18 @@ import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './routes/auth';
 import trim from './middleware/trim';
+import dotenv = require('dotenv');
+import cookieParser = require("cookie-parser");
+
+//carga variables de entorno para configuración
+dotenv.config({path: 'config/dev.env'});
 
 const app = express();
 
 app.use(express.json())
 app.use(morgan('dev'));
 app.use(trim);
+app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('Hola mundo'));
 app.use('/api/auth', authRoutes)
