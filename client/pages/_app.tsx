490f0dcef703c372837e7bd00e91085b1e9af002
Cristian Plasencia <plc.cristian@gmail.com>
2021-04-03 21:21:04 +0100

    componente navbar finalizado

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,22 @@
-import '../styles/globals.css'
+import '../styles/tailwind.css'
 import { AppProps } from "next/app";
-import Axios from "axios";
+import Axios from "Axios";
+import Navbar from '../components/navbar';
+import { Fragment } from 'react';
+import { useRouter } from 'next/router'
 
 //Axios.defaults.baseURL = 'http://localhost:500/api';
 Axios.defaults.withCredentials = true
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { pathname } = useRouter() //comprueba la ruta y si esta en login/registro no muestra navbar
+  const authRoutes = ['/register', '/login']
+  const authRoute = authRoutes.includes(pathname)
+  
+  return <Fragment>
+    {!authRoute ? <Navbar /> : ''}
+    <Component {...pageProps} />
+  </Fragment>
 }
 
 export default App
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,36 +1,12 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-//import Logo from '../images/logo.svg';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='bg-gray-100'>
       <Head>
         <title>Teselar: bla bla bla</title>
       </Head>
 
-      <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-center h-12 bg-white">
-        {/* Logo y título */}
-        <div className='flex items-center'>
-          <Link href='/'>
-            <a>
-              {/*<Logo classname='w-8 h-8 mr-2' />*/} {/* SVG LOGO */}
-            </a>
-          </Link>
-          <span className='font-semibold text-2x1'>
-            <Link href='/'>Teselar</Link>
-          </span>
-        </div>
-
-        {/* Busqueda */}
-        <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
-          <i className='pl-4 pr-3 text-gray-500 fas fa-search' />
-          <input type="text" className='py-1 pr-3 bg-transparent rounded w-160 focus:outline-none'/>
-        </div>
-
-        {/* botones login-registro */}
-      </div>
     </div>
   )
 }
