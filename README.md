46e870d45e0818724943081f5eb3fce0fae341c6
Cristian Plasencia <plc.cristian@gmail.com>
2021-04-19 20:20:19 +0100

    recupera posts de la base de datos y los muestra

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Steps to run this project:
 1. Run `npm i` command
 2. Setup database settings inside `ormconfig.json` file
 3. Run `npm start` command
+
+Client: npm run dev
+Server: npm start
+Database: psql -U usuario
+Connect DB: \c databasename
+Tables: \dt
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,41 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import Axios from 'Axios';
+import { Post } from '../types';
 
 export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
+  
+  useEffect(() => {
+    Axios.get('/posts')
+      .then(res => setPosts(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <div>
+    <div className='pt-12'>
       <Head>
         <title>Teselar: bla bla bla</title>
       </Head>
-
+      <div className='container flex pt-4'>
+        {/* entradas */}
+        <div className='w-160'>
+          <h1>Envíos recientes</h1>
+          {posts.map(post => (
+            <div key={post.identifier} className='flex mb-4 bg-white rounded'>
+              {/* Votos */}
+              <div className="w-10 text-center bg-gray-200 rounded-l">
+                <p>V</p>
+              </div>
+              {/* Post */}
+              <div className='w-full p-2'>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* sidebar */}
+      </div>
     </div>
-  )
+  );
 }
