d07e76de952f277a9275b6f901dcb741d6c36e8b
Cristian Plasencia <plc.cristian@gmail.com>
2021-05-13 18:34:56 +0100

    añadir imagenes de comunidades en frontend

--- client/pages/t/[sub].tsx
+++ client/pages/t/[sub].tsx
@@ -1,33 +1,80 @@
-import { useRouter } from "next/router";
-import useSWR from "swr";
-import PostCard from "../../components/PostCard";
+import { Head } from 'next/document';
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import useSWR from 'swr';
+import PostCard from '../../components/PostCard';
+import { Sub } from '../../types';
+import Image from 'next/image';
 
-export default function Sub() {
-    const router = useRouter()
+export default function SubPage() {
+  const router = useRouter();
 
-    const subName = router.query.sub
+  const subName = router.query.sub;
 
-    const { data: sub, error } = useSWR(subName ? `/subs/${subName}` : null)
+  const { data: sub, error } = useSWR<Sub>(subName ? `/subs/${subName}` : null);
 
-    //si la comunidad no existe, redirigue a inicio
-    if(error) router.push('/')
+  //si la comunidad no existe, redirigue a inicio
+  if (error) router.push('/');
 
-    let postsMarkup
-    if(!sub) {
-        postsMarkup = <p className='text-lg text-center'>Cargando...0</p>
-    } else if (sub.posts.length === 0){
-        postsMarkup = <p className='text-lg text-center'>Aún no se ha creado ninguna entrada</p>
-    } else {
-        postsMarkup = sub.posts.map(post => <PostCard key={post.id} post={post} />)
-    }
+  let postsMarkup;
+  if (!sub) {
+    postsMarkup = <p className='text-lg text-center'>Cargando...0</p>;
+  } else if (sub.posts.length === 0) {
+    postsMarkup = (
+      <p className='text-lg text-center'>Aún no se ha creado ninguna entrada</p>
+    );
+  } else {
+    postsMarkup = sub.posts.map((post) => (
+      <PostCard key={post.id} post={post} />
+    ));
+  }
 
-    return (
-        <div className='container flex pt-5'>
-            {sub && (
-            <div className='w-160'>
-                {postsMarkup}
+  return (
+    <div>
+      <Head>
+        <title>{sub?.title}</title>
+      </Head>
+
+      {sub && (
+        <Fragment>
+          {/* Sub info and images */}
+          <div>
+            {/* Banner imagen */}
+            <div className='bg-blue-500'>
+              {sub.bannerUrl ? (
+                <div
+                  className='h-56 bg-blue-500'
+                  style={{
+                    backgroundImage: `url(${sub.bannerUrl})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                ></div>
+              ) : (
+                <div className='h-20 bg-blue-500'></div>
+              )}
             </div>
-            )}
-        </div>
-    )
+            {/* Sub meta data*/}
+            <div className='h-20 bg-white'>
+              <div className='container flex'>
+                <Image
+                  src={sub.imageUrl}
+                  alt='Sub'
+                  className='rounded-full'
+                  width={80}
+                  height={80}
+                ></Image>
+              </div>
+            </div>
+          </div>
+          {/* Posts y Sidebar */}
+          <div className='container flex pt-5'>
+            <div className='w-160'></div>
+            {postsMarkup}
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
 }
