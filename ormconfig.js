9eb9d8b1201329e0bc0df8b0939122dbb6845bde
Cristian Plasencia <plc.cristian@gmail.com>
2021-08-03 21:47:27 +0100

    updated ormconfig.json to ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
-{
-   "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
-   "username": "postgres",
-   "password": "root",
-   "database": "teselar",
+module.exports = {
+   "type": process.env.DB_DIALECT,
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USERNAME,
+   "password": process.env.DB_PASSWORD,
+   "database": process.env.DB_DATABASE,
    "synchronize": true,
    "logging": true,
    "entities": [
